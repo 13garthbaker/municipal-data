3ef1ea5c68503c469a01c5a6b6746b3fda351187
Jurie-Jan Botha <juriejanbotha@gmail.com>
2020-12-14 09:49:14 +0200

    Using timestamp for update file names

--- municipal_finance/models/updates.py
+++ municipal_finance/models/updates.py
@@ -4,6 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def upload_to(path):
+    def get_file_path(instance, filename):
+        ext = filename.split(".")[-1]
+        timestamp = instance.datetime.strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"{path}{timestamp}.{ext}"
+        return filename
+    return get_file_path
+
+
 class BaseUpdate(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, models.DO_NOTHING)
@@ -17,7 +26,7 @@ class BaseUpdate(models.Model):
 
 class MunicipalStaffContactsUpdate(BaseUpdate):
     file = models.FileField(
-        upload_to='updates/municipal_staff_contacts/',
+        upload_to=upload_to('updates/municipal_staff_contacts/'),
         max_length=255,
     )
 
@@ -27,7 +36,7 @@ class MunicipalStaffContactsUpdate(BaseUpdate):
 
 class IncomeExpenditureV2Update(BaseUpdate):
     file = models.FileField(
-        upload_to='updates/income_expenditure_v2/',
+        upload_to=upload_to('updates/income_expenditure_v2/'),
         max_length=255,
     )
 
@@ -37,7 +46,7 @@ class IncomeExpenditureV2Update(BaseUpdate):
 
 class CashFlowV2Update(BaseUpdate):
     file = models.FileField(
-        upload_to='updates/cash_flow_v2/',
+        upload_to=upload_to('updates/cash_flow_v2/'),
         max_length=255,
     )
 
@@ -47,7 +56,7 @@ class CashFlowV2Update(BaseUpdate):
 
 class RepairsMaintenanceV2Update(BaseUpdate):
     file = models.FileField(
-        upload_to='updates/repairs_maintenance_v2/',
+        upload_to=upload_to('updates/repairs_maintenance_v2/'),
         max_length=255,
     )
 
